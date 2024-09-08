eefe04cf789a0aa53050ff0da901b89054f4a19c
slowboard <zynix976@gmail.com>
2024-09-08 12:55:26 +0200

    Updated main file. Tested on 3.10.9

--- BoiledLeather.py
+++ BoiledLeather.py
@@ -1,71 +1,75 @@
-import os, re, shutil,sys,fileinput
+# The script assumes that the files for each book is in separate subfolders the folder this script is in.
+# So if the script is at "C:\Users\YourUserName\Download\boiled_leather.py", then the script assumes the
+# book files are in "C:\Users\YourUserName\Download\AFFC" and "C:\Users\YourUserName\Download\ADWD".
 
-book = ["AFFC","ADWD"]			# The book names. Don't change these
-start = [7,5]					# The number of the prologue .html files
-og_file = "index_split"			# Original filename "index_split_xxx.html"
-padding = 3						# number of padded zeros
-length = [46, 73]				# Number of chapters in each book
-# stop = [52,77]
+# This script also assumes the files follow the format:
+# {original_file_prefix}{chapter_number_with_padding}.{file_extension}
+# 
+# For me, the files are called "index_split_xxx.html", so the variables should be:
+original_file_prefix = "index_split_"
+original_file_extension = "html"
+padding = 3
 
-path = os.getcwd()				# Gets the path to the work folder (where this script is)
-outfolder = "%s\\book\\" % path	# Generates path to the output folder
+# The script also assumes that there might be a number of pre-amble files.
+# So the book starts at a number other than 1. In my case, the AFFC prologue
+# is "index_split_007.html", and the ADWD prologue is "index_split_005.html"
+# Therefore the prologue_counts is 7 and 5:
+prologue_counts = [7,5]
+
+# The output files will be placed in the subfolder "book", with the following format:
+# "{boiled_leather_chapter_number} - {original_chapter}.{file_extension}"
+# For example: "Jaime I" is the AFFC chapter 9, and boiled leather chapter 17, so it will be named
+# "17 - AFFC 9.{file_extension}"
+
+import shutil
+import pathlib
+
+books = ["AFFC", "ADWD"] # The book names
+chapter_count = [46, 73] # Number of chapters in each book
+
+script_path = pathlib.Path(__file__)
+cwd = script_path.parent
+output_folder = cwd / 'book' 
+if not output_folder.exists():
+    output_folder.mkdir()
 
 
-
-# Getting a list of the chapters
-f = open("%s\\chapters.txt" % path)
-s = f.read()
-
+# Getting a list of the chapters. They are in the format "{Chapter name}: {Book} {chapter number}"
+# We only want the "{Book} {chapter number}" part
 chapters = []
-string = re.split(":|\n",s)
-
-for i in string:
-	if "ADWD" in i or "AFFC" in i:
-		chapters.append(i[1:])
+with open(cwd / 'chapters.txt', 'r', encoding='utf-8') as f:
+    file_contents = f.read()
+    for line in file_contents.split('\n'):
+        title, book_and_chapter_number = line.split(': ')
+        chapters.append(book_and_chapter_number)
 
 
 # Copying and renaming files
-for index, bookname in enumerate(book):
+for book_index, book_name in enumerate(books):
 
-	# Folder name for original files
-	folder = "%s\\%s\\" % (path,bookname)
+    # Folder name for original files
+    folder = cwd / book_name
 
-	# Copying and renaming chapters
-	for i in range(start[index],start[index]+length[index]):
-		# Getting original filename
-		num = str(i).zfill(padding)
-		og_filename = "%s%s_%s.html" % (folder, og_file, num)
-		# new filename
-		temp_name = "%s%s %d.html" % (outfolder, bookname, i-start[index]+1)
-		shutil.copy(og_filename,temp_name)
+    # Start and end number for the files of the book
+    book_start_number = prologue_counts[book_index]
+    book_end_number = book_start_number + chapter_count[book_index]
+
+    # Copying and renaming chapters
+    for i in range(book_start_number, book_end_number):
+
+        # Getting file number, with padding
+        file_number = str(i).zfill(padding)
+        original_filename = folder / f"{original_file_prefix}{file_number}.{original_file_extension}"
+
+        # new filename
+        new_chapter_number = i + 1 - prologue_counts[book_index]
+        temp_name = output_folder / f"{book_name} {new_chapter_number}.{original_file_extension}"
+        shutil.copy(original_filename, temp_name)
 
 
 # Renaming chapters to fit boiled leather semi-chronological order
 for num, name in enumerate(chapters):
-	filename = "%s%s.html" % (outfolder, name)
-	newfile = "%s%d - %s.html" % (outfolder, num+1, name)
-	os.rename(filename,newfile)
+    current_filename = output_folder / f"{name}.{original_file_extension}"
+    new_filename = output_folder / f"{num+1} - {name}.{original_file_extension}"
+    current_filename.rename(new_filename)
 
-
-
-# Things that don't really work or I don't use
-# Part of the main for loop, for moving the appendix. Does work, but I don't use it
-# appendix = book[1]
-# app_start = 78
-# app_stop = 99
-	# Copying and moving appendecies
-	# if bookname == appendix:
-	# 	for i in range(app_start,app_stop+1):
-	# 		og_filename = "%s%s_%03d.html" % (folder, og_file, i)
-
-	# 		temp_name = "%sappendix %d.html" % (outfolder, i-app_start+1)
-	# 		shutil.copy(og_filename,temp_name)
-
-# Renaming stylesheets for use of two seperate stylesheets. Doesn't work.
-# for i in range(stop[1]-start[1]):
-# 	searchExp = """  <link href="styles/stylesheet.css" rel="stylesheet" type="text/css"/>"""
-# 	replaceExp = """  <link href="styles/stylesheet1.css" rel="stylesheet" type="text/css"/>"""
-# 	name = "%s%s %d.html" % (outfolder, book[1], i)
-# 	for line in fileinput.input(name, inplace = 1):
-# 		if searchExp in line:
-# 			line = line.replace(searchExp,replaceExp)
